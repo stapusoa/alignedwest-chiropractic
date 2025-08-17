3a197acf8c214960a8ff87baf89018f2610d6bf9
Sara Tapusoa <79812543+stapusoa@users.noreply.github.com>
2025-08-17 16:36:11 -0600

    adjust widths to fractions

--- apps/web/src/components/hero.tsx
+++ apps/web/src/components/hero.tsx
@@ -36,12 +36,12 @@ export function Hero({
       />
       <div className="flex relative w-full items-center -gap-2 max-w-6xl">
         {/* Text / content left-aligned */}
-        <div className="flex relative z-20 w-[80%] px-8 text-left">
+        <div className="flex relative z-20 w-2/5 px-8 text-left">
           {children}
         </div>
 
         {/* ScrollHero Animation on the right */}
-        <div className="flex relative top-30 right-0 h-full w-[130%] z-0">
+        <div className="flex relative top-30 right-0 h-full w-3/5 z-0">
           <ScrollHero />
         </div>
       </div>
